3e04015b75f1085b4b1d9e8065a24610328ee2ac
Daniel Nastase <dnastase@gmail.com>
2016-12-14 17:44:50 -0500

    added more requests/answers; docstrings for some API req/ans

---
+++ commission_report.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+"""
+Copyright (C) 2016 Interactive Brokers LLC. All rights reserved.  This code is
+subject to the terms and conditions of the IB API Non-Commercial License or the
+ IB API Commercial License, as applicable. 
+"""
+
+from utils import Object 
+
+
+class CommissionReport(Object):
+
+    def __init__(self):
+        self.execId = ""
+        self.commission = 0. 
+        self.currency = ""
+        self.realizedPNL =  0.
+        self.yield_ = 0.
+        self.yieldRedemptionDate = 0  # YYYYMMDD format
+ 
